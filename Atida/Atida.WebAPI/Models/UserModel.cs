325ce75848bca524f6293d64a8707ac2bc938575
ayala-shtraicher <a0504169337@gmail.com>
2023-05-09 14:14:14 +0300

    commit9

--- Atida/Atida.WebAPI/Models/UserModel.cs
+++ Atida/Atida.WebAPI/Models/UserModel.cs
@@ -5,18 +5,17 @@ namespace Atida.WebAPI.Models
     public class UserModel
     {
         public int Id { get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [Required]
-        //[StringLength(9,ErrorMessage ="Tz length can't be more than 9")]
-       // [MinLength(9,ErrorMessage = "Tz length can't be less than 9")]
+        [StringLength(9,ErrorMessage ="Tz length can't be more than 9")]
+        [MinLength(9,ErrorMessage = "Tz length can't be less than 9")]
         public string Tz { get; set; }
         public AddressModel Address { get; set; }
         public DateTime DateOfBirth { get; set; }
-       // [Phone]
+        [Phone]
         public string Phone { get; set; }
-      //  [Phone]
+        [Phone]
         public string MobilePhone { get; set; }
         public UserModel()
         {
